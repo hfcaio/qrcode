8d7df9263ec4a9092a63e516e3c1ac55c4b1f3f3
hfcaio <hfcaio@gmail.com>
2023-07-16 00:02:40 -0300

    change image in detector-image

--- detector-image.py
+++ detector-image.py
@@ -2,7 +2,7 @@ import cv2
 
 qcd = cv2.QRCodeDetector()
 
-img = cv2.imread("qrcode-example.JPG", -1)
+img = cv2.imread("4_qrcode.png", -1)
 
 retval, decoded_info, points, straight_qrcode = qcd.detectAndDecodeMulti(img)
 if retval:
@@ -13,11 +13,10 @@ img = cv2.polylines(img, points.astype(int), True, (0, 255, 0), 8)
 
 
 for s, p in zip(decoded_info, points):
-    img = cv2.putText(img, s, p[0].astype(int),
+    img = cv2.putText(img, s, (p[0].astype(int)[0], p[0].astype(int)[1] - 10),
                       cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
 
-img2 = cv2.resize(img, (800, 900))
-cv2.imshow("test", img2)
+cv2.imshow("test", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
